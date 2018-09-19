2653019beb054474a270a241e648d99b5b5d66d3
kondax <kondaxsolutions@gmail.com>
2018-09-20 00:12:03 +0100

    Configure testing to use node scripts

--- test/TronWeb Class.js
+++ test/TronWeb Class.js
@@ -15,14 +15,15 @@ const createInstance = () => {
     return new TronWeb(FULL_NODE_API, SOLIDITY_NODE_API, EVENT_API, PRIVATE_KEY);
 }
 
-describe('TronWeb Instance', () => {
-    describe('#constructor()', () => {
-        it('should create a full instance', () => {
+describe('TronWeb Instance', function () {
+
+    describe('#constructor()', function () {
+        it('should create a full instance', function () {
             const tronWeb = createInstance();
             assert.instanceOf(tronWeb, TronWeb);
         });
 
-        it('should create an instance without a private key', () => {
+        it('should create an instance without a private key', function () {
             const fullNode = new HttpProvider(FULL_NODE_API);
             const solidityNode = new HttpProvider(SOLIDITY_NODE_API);
             const eventServer = EVENT_API;
@@ -36,7 +37,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultPrivateKey, false);
         });
 
-        it('should create an instance without an event server', () => {
+        it('should create an instance without an event server', function () {
             const fullNode = new HttpProvider(FULL_NODE_API);
             const solidityNode = new HttpProvider(SOLIDITY_NODE_API);
 
@@ -48,7 +49,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.eventServer, false);
         });
 
-        it('should reject an invalid full node URL', () => {
+        it('should reject an invalid full node URL', function () {
             const solidityNode = new HttpProvider(SOLIDITY_NODE_API);
 
             assert.throws(() => new TronWeb(
@@ -57,7 +58,7 @@ describe('TronWeb Instance', () => {
             ), 'Invalid URL provided to HttpProvider');
         });
 
-        it('should reject an invalid solidity node URL', () => {
+        it('should reject an invalid solidity node URL', function () {
             const fullNode = new HttpProvider(FULL_NODE_API);
 
             assert.throws(() => new TronWeb(
@@ -66,7 +67,7 @@ describe('TronWeb Instance', () => {
             ), 'Invalid URL provided to HttpProvider');
         });
 
-        it('should reject an invalid event server URL', () => {
+        it('should reject an invalid event server URL', function () {
             const fullNode = new HttpProvider(FULL_NODE_API);
             const solidityNode = new HttpProvider(SOLIDITY_NODE_API);
 
@@ -78,8 +79,8 @@ describe('TronWeb Instance', () => {
         });
     });
 
-    describe('#setDefaultBlock()', () => {
-        it('should accept a positive integer', () => {
+    describe('#setDefaultBlock()', function () {
+        it('should accept a positive integer', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock(1);
@@ -87,7 +88,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultBlock, 1);
         });
 
-        it('should correct a negative integer', () => {
+        it('should correct a negative integer', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock(-2);
@@ -95,15 +96,15 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultBlock, 2);
         });
 
-        it('should accept 0', () => {
+        it('should accept 0', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock(0);
 
             assert.equal(tronWeb.defaultBlock, 0);
         });
-        
-        it('should be able to clear', () => {
+
+        it('should be able to clear', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock();
@@ -111,7 +112,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultBlock, false);
         });
 
-        it('should accept "earliest"', () => {
+        it('should accept "earliest"', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock('earliest');
@@ -119,7 +120,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultBlock, 'earliest');
         });
 
-        it('should accept "latest"', () => {
+        it('should accept "latest"', function () {
             const tronWeb = createInstance();
 
             tronWeb.setDefaultBlock('latest');
@@ -127,21 +128,21 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultBlock, 'latest');
         });
 
-        it('should reject a decimal', () => {
+        it('should reject a decimal', function () {
             const tronWeb = createInstance();
 
             assert.throws(() => tronWeb.setDefaultBlock(10.2), 'Invalid block ID provided');
         });
 
-        it('should reject a string', () => {
+        it('should reject a string', function () {
             const tronWeb = createInstance();
 
             assert.throws(() => tronWeb.setDefaultBlock('test'), 'Invalid block ID provided');
         });
     });
 
-    describe('#setPrivateKey()', () => {
-        it('should accept a private key', () => {
+    describe('#setPrivateKey()', function () {
+        it('should accept a private key', function () {
             const tronWeb = new TronWeb(FULL_NODE_API, SOLIDITY_NODE_API, EVENT_API);
 
             tronWeb.setPrivateKey(PRIVATE_KEY);
@@ -149,7 +150,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultPrivateKey, PRIVATE_KEY);
         });
 
-        it('should set the appropriate address for the private key', () => {
+        it('should set the appropriate address for the private key', function () {
             const tronWeb = new TronWeb(FULL_NODE_API, SOLIDITY_NODE_API, EVENT_API);
 
             tronWeb.setPrivateKey(PRIVATE_KEY);
@@ -158,15 +159,15 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultAddress.base58, ADDRESS_BASE58);
         });
 
-        it('should reject an invalid private key', () => {
+        it('should reject an invalid private key', function () {
             const tronWeb = new TronWeb(FULL_NODE_API, SOLIDITY_NODE_API, EVENT_API);
 
             assert.throws(() => tronWeb.setPrivateKey('test'), 'Invalid private key provided');
         });
     });
 
-    describe('#setAddress()', () => {
-        it('should accept a hex address', () => {
+    describe('#setAddress()', function () {
+        it('should accept a hex address', function () {
             const tronWeb = createInstance();
 
             tronWeb.setAddress(ADDRESS_HEX);
@@ -175,7 +176,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultAddress.base58, ADDRESS_BASE58);
         });
 
-        it('should accept a base58 address', () => {
+        it('should accept a base58 address', function () {
             const tronWeb = createInstance();
 
             tronWeb.setAddress(ADDRESS_BASE58);
@@ -184,7 +185,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultAddress.base58, ADDRESS_BASE58);
         });
 
-        it('should reset the private key if the address doesn\'t match', () => {
+        it('should reset the private key if the address doesn\'t match', function () {
             const tronWeb = createInstance();
 
             assert.equal(tronWeb.defaultPrivateKey, PRIVATE_KEY);
@@ -198,32 +199,36 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.defaultAddress.base58, 'TPL66VK2gCXNCD7EJg9pgJRfqcRbnn4zcp');
         });
 
-        it('should not reset the private key if the address matches', () => {
+        it('should not reset the private key if the address matches', function () {
             const tronWeb = createInstance();
 
             tronWeb.setAddress(ADDRESS_BASE58);
-            
+
             assert.equal(tronWeb.defaultPrivateKey, PRIVATE_KEY);
         });
     });
 
-    describe('#isValidProvider()', () => {
-        it('should accept a valid provider', () => {
+    describe('#isValidProvider()', function () {
+        it('should accept a valid provider', function () {
             const tronWeb = createInstance();
             const provider = new HttpProvider(FULL_NODE_API);
 
             assert.equal(tronWeb.isValidProvider(provider), true);
         });
 
-        it('should accept an invalid provider', () => {
+        it('should accept an invalid provider', function () {
             const tronWeb = createInstance();
 
             assert.equal(tronWeb.isValidProvider('test'), false);
         });
     });
 
-    describe('#isEventServerConnected()', () => {
+    describe('#isEventServerConnected()', function () {
+
+        this.timeout(6000);
+
         it('should return true for valid event server', async () => {
+
             const tronWeb = createInstance();
             const isConnected = await tronWeb.isEventServerConnected();
 
@@ -231,6 +236,7 @@ describe('TronWeb Instance', () => {
         });
 
         it('should return false for invalid event server', async () => {
+
             const tronWeb = createInstance();
 
             tronWeb.setEventServer('https://google.co.uk');
@@ -239,10 +245,19 @@ describe('TronWeb Instance', () => {
 
             assert.equal(isConnected, false);
         });
+
+        it('should return false if the eventServer is null', async () => {
+            const tronWeb = createInstance();
+            tronWeb.eventServer = null
+
+            const isConnected = await tronWeb.isEventServerConnected();
+
+            assert.equal(isConnected, false);
+        });
     });
 
-    describe('#setFullNode()', () => {
-        it('should accept a HttpProvider instance', () => {
+    describe('#setFullNode()', function () {
+        it('should accept a HttpProvider instance', function () {
             const tronWeb = createInstance();
             const provider = new HttpProvider(FULL_NODE_API);
 
@@ -251,7 +266,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.fullNode, provider);
         });
 
-        it('should accept a valid URL string', () => {
+        it('should accept a valid URL string', function () {
             const tronWeb = createInstance();
             const provider = FULL_NODE_API;
 
@@ -260,21 +275,21 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.fullNode.host, provider);
         });
 
-        it('should reject a non-string', () => {
+        it('should reject a non-string', function () {
             assert.throws(() => {
                 createInstance().setFullNode(true)
             }, 'Invalid full node provided');
         });
 
-        it('should reject an invalid URL string', () => {
+        it('should reject an invalid URL string', function () {
             assert.throws(() => {
                 createInstance().setFullNode('test')
             }, 'Invalid URL provided to HttpProvider');
         });
     });
 
-    describe('#setSolidityNode()', () => {
-        it('should accept a HttpProvider instance', () => {
+    describe('#setSolidityNode()', function () {
+        it('should accept a HttpProvider instance', function () {
             const tronWeb = createInstance();
             const provider = new HttpProvider(SOLIDITY_NODE_API);
 
@@ -283,7 +298,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.solidityNode, provider);
         });
 
-        it('should accept a valid URL string', () => {
+        it('should accept a valid URL string', function () {
             const tronWeb = createInstance();
             const provider = SOLIDITY_NODE_API;
 
@@ -292,21 +307,21 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.solidityNode.host, provider);
         });
 
-        it('should reject a non-string', () => {
+        it('should reject a non-string', function () {
             assert.throws(() => {
                 createInstance().setSolidityNode(true)
             }, 'Invalid solidity node provided');
         });
 
-        it('should reject an invalid URL string', () => {
+        it('should reject an invalid URL string', function () {
             assert.throws(() => {
                 createInstance().setSolidityNode('test')
             }, 'Invalid URL provided to HttpProvider');
         });
     });
 
-    describe('#setEventServer()', () => {
-        it('should accept a valid URL string', () => {
+    describe('#setEventServer()', function () {
+        it('should accept a valid URL string', function () {
             const tronWeb = createInstance();
             const eventServer = EVENT_API;
 
@@ -315,7 +330,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.eventServer, eventServer);
         });
 
-        it('should reset the event server property', () => {
+        it('should reset the event server property', function () {
             const tronWeb = createInstance();
 
             tronWeb.setEventServer(false);
@@ -323,7 +338,7 @@ describe('TronWeb Instance', () => {
             assert.equal(tronWeb.eventServer, false);
         });
 
-        it('should reject an invalid URL string', () => {
+        it('should reject an invalid URL string', function () {
             const tronWeb = createInstance();
 
             assert.throws(() => {
@@ -331,7 +346,7 @@ describe('TronWeb Instance', () => {
             }, 'Invalid URL provided for event server');
         });
 
-        it('should reject an invalid URL parameter', () => {
+        it('should reject an invalid URL parameter', function () {
             const tronWeb = createInstance();
 
             assert.throws(() => {
@@ -340,8 +355,8 @@ describe('TronWeb Instance', () => {
         });
     });
 
-    describe('#currentProviders()', () => {
-        it('should return the current providers', () => {
+    describe('#currentProviders()', function () {
+        it('should return the current providers', function () {
             const tronWeb = createInstance();
             const providers = tronWeb.currentProviders();
 
@@ -350,4 +365,16 @@ describe('TronWeb Instance', () => {
             assert.equal(providers.eventServer, EVENT_API);
         });
     });
+
+    describe('#currentProvider()', function () {
+        it('should return the current providers', function () {
+            const tronWeb = createInstance();
+            const providers = tronWeb.currentProvider();
+
+            assert.equal(providers.fullNode.host, FULL_NODE_API);
+            assert.equal(providers.solidityNode.host, SOLIDITY_NODE_API);
+            assert.equal(providers.eventServer, EVENT_API);
+        });
+    });
+
 });
