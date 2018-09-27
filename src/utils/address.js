f83d26af294e0d8214ca5530cd0f1223baa2c7eb
Tian Han <tian@tron.network>
2018-09-26 17:30:40 -0700

    deleting deprecated address formats

--- src/utils/address.js
+++
@@ -1,5 +0,0 @@
-const isTestNet = process.env.NET === 'testnet';
-
-export const ADDRESS_SIZE = isTestNet ? 35 : 34;
-export const ADDRESS_PREFIX = isTestNet ? "a0" : "41";
-export const ADDRESS_PREFIX_BYTE = isTestNet ? 0xa0 : 0x41;
