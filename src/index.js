c737c47b963573d600c8def7f6927c895b473b22
Shamsudin <steein.shamsudin@gmail.com>
2018-11-05 20:44:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -57,8 +57,11 @@ export default class TronWeb extends EventEmitter {
     }
 
     setDefaultBlock(blockID = false) {
-        if(blockID === false || blockID == 'latest' || blockID == 'earliest' || blockID === 0)
+//         if(blockID === false || blockID == 'latest' || blockID == 'earliest' || blockID === 0)
+//             return this.defaultBlock = blockID;
+        if([ false, 'latest', 'earliest', 0 ].includes(blockID)) {
             return this.defaultBlock = blockID;
+        }
 
         if(!utils.isInteger(blockID) || !blockID)
             throw new Error('Invalid block ID provided');
