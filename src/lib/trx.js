9fe5b86fb345c7fba51026a89700408ef77649ef
Francesco Sullo <sullof@gmail.com>
2018-11-22 13:05:35 -0800

    2.1.9

--- src/lib/trx.js
+++ src/lib/trx.js
@@ -656,7 +656,8 @@ export default class Trx {
             signedTransaction,
             'post'
         ).then(result => {
-            result.signedTransaction = signedTransaction;
+            if (result.result)
+                result.signedTransaction = signedTransaction;
             callback(null, result);
             if (result.result) {
                 const timeout = Date.now() + 6e4 // 1 minutes
