67745140f45b5c71bf28948cdfb97bac2ae9e0b8
Francesco Sullo <sullof@gmail.com>
2019-05-06 14:05:37 -0700

    Adding SKIP_TESTS to pre-push

--- scripts/pre-push.js
+++ scripts/pre-push.js
@@ -1,3 +1,5 @@
+
+
 const {execSync, spawn} = require('child_process')
 const chalk = require('chalk')
 
@@ -13,35 +15,38 @@ try {
 
 let errors = false
 
-const build = spawn('yarn', ['build'])
+if (!process.env.SKIPTESTS) {
 
-build.stdout.on('data', function (data) {
-    process.stdout.write(data.toString())
-})
+    const build = spawn('yarn', ['build'])
 
-build.stderr.on('data', function (data) {
-    process.stdout.write(data.toString())
-})
-
-build.on('exit', function (code) {
-    const test = spawn('yarn', ['test'])
-
-    test.stdout.on('data', function (data) {
+    build.stdout.on('data', function (data) {
         process.stdout.write(data.toString())
     })
 
-    test.stderr.on('data', function (data) {
-        errors = true
-        console.log('stderr: ' + data.toString())
+    build.stderr.on('data', function (data) {
+        process.stdout.write(data.toString())
     })
 
-    test.on('exit', function (code) {
-        if (errors) {
-            console.log(chalk.red('Tests have failed. Please verify tests are passing before pushing'));
-            process.exit(1);
-        }
+
+    build.on('exit', function (code) {
+        const test = spawn('yarn', ['test'])
+
+        test.stdout.on('data', function (data) {
+            process.stdout.write(data.toString())
+        })
+
+        test.stderr.on('data', function (data) {
+            errors = true
+            console.log('stderr: ' + data.toString())
+        })
+
+        test.on('exit', function (code) {
+            if (errors) {
+                console.log(chalk.red('Tests have failed. Please verify tests are passing before pushing'));
+                process.exit(1);
+            }
+        })
     })
-})
 
-
+}
 
