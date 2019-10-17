c93e1483764b70a2c66562a9909052c266115e9f
Francesco Sullo <sullof@gmail.com>
2019-10-17 15:50:31 -0700

    Adding external package for injectpromise

--- test/helpers/BlockLib.js
+++ test/helpers/BlockLib.js
@@ -1,17 +1,5 @@
 
-function injectPromise(scope) {
-    return (func, ...args) => {
-        return (function (func, ...args) {
-            return new Promise((resolve, reject) => {
-                func(...args, (err, res) => {
-                    if (err)
-                        reject(err);
-                    else resolve(res);
-                });
-            });
-        })(func.bind(scope), ...args);
-    }
-}
+const injectPromise = require('injectpromise')
 
 class BlockLib {
 
