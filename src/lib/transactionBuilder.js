3536132532861c2cb24d7dc9a5b8e4148747216f
stan <stan@gmail.com>
2023-04-05 22:25:28 +0800

    feat: remove Permission_id if it's undefined or 0

--- src/lib/transactionBuilder.js
+++ src/lib/transactionBuilder.js
@@ -56,31 +56,31 @@ function getHeaderInfo(node) {
         });
 }
 
-function createTransaction(tronWeb, type, value, Permission_id, options = {}) {
-    return getHeaderInfo(tronWeb.fullNode)
-        .then((metaData) => {
-            const tx = {
-                visible: false,
-                txID: '',
-                raw_data_hex: '',
-                raw_data: {
-                    contract: [{
-                        parameter: {
-                            value,
-                            type_url: `type.googleapis.com/protocol.${type}`,
-                        },
-                        Permission_id,
-                        type,
-                    }],
-                    ...metaData,
-                    ...options,
+async function createTransaction(tronWeb, type, value, Permission_id, options = {}) {
+    const metaData = await getHeaderInfo(tronWeb.fullNode)
+    const tx = {
+        visible: false,
+        txID: '',
+        raw_data_hex: '',
+        raw_data: {
+            contract: [{
+                parameter: {
+                    value,
+                    type_url: `type.googleapis.com/protocol.${type}`,
                 },
-            };
-            const pb = txJsonToPb(tx);
-            tx.txID = txPbToTxID(pb).replace(/^0x/, '');
-            tx.raw_data_hex = txPbToRawDataHex(pb).toLowerCase();
-            return tx;
-        });
+                type,
+            }],
+            ...metaData,
+            ...options,
+        },
+    };
+    if (Permission_id) {
+        tx.raw_data.contract[0].Permission_id = Permission_id;
+    }
+    const pb = txJsonToPb(tx);
+    tx.txID = txPbToTxID(pb).replace(/^0x/, '');
+    tx.raw_data_hex = txPbToRawDataHex(pb).toLowerCase();
+    return tx;
 }
 
 export default class TransactionBuilder {
