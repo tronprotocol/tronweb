c5ceb01e13fa04eb1940c25c5d0ef9e8c9517f3f
stan <stan@gmail.com>
2023-06-12 16:56:27 +0800

    feat: remove redundant packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,7 @@ const path = require('path');
 const externals = require('webpack-node-externals');
 
 const basePlugins = [
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-object-rest-spread'
 ];
 
 const mode = process.env.NODE_ENV || 'production'
