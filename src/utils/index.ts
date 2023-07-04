79873353a51b5567c692aef20a0e515787bc681e
cathy.tan <cathy.tan@tron.network>
2023-07-04 14:44:23 +0800

    feat: support ts for paramValidator

---
+++ src/utils/index.ts
@@ -0,0 +1,26 @@
+// import * as accounts from "./accounts";
+// import * as base58 from "./base58";
+// import * as bytes from "./bytes";
+// import * as crypto from "./crypto";
+// import * as code from "./code";
+// import * as abi from "./abi";
+// import * as message from "./message";
+// import * as ethersUtils from "./ethersUtils";
+// import { TypedDataEncoder as _TypedDataEncoder } from "./typedData";
+// import * as transaction from "./transaction";
+
+import * as validations from "./validations";
+
+export default {
+    ...validations,
+    // code,
+    // accounts,
+    // base58,
+    // bytes,
+    // crypto,
+    // abi,
+    // message,
+    // _TypedDataEncoder,
+    // transaction,
+    // ethersUtils,
+};
