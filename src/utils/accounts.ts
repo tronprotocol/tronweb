424e2779ea9b18417dc8d0b87c551cfdbabfc7f9
startan <star.tan@tron.network>
2023-07-04 14:42:35 +0800

    feat: add utils

---
+++ src/utils/accounts.ts
@@ -0,0 +1,77 @@
+import { byteArray2hexStr } from "./bytes";
+import {
+  getBase58CheckAddress,
+  genPriKey,
+  getAddressFromPriKey,
+  getPubKeyFromPriKey,
+  pkToAddress,
+} from "./crypto";
+import {
+  ethersWallet,
+  ethersHDNodeWallet,
+  Mnemonic,
+  Wordlist,
+} from "./ethersUtils";
+import { TRON_BIP39_PATH_INDEX_0 } from "./address";
+
+const INVALID_TRON_PATH_ERROR_MSG = "Invalid tron path provided";
+
+export function generateAccount() {
+  const priKeyBytes = genPriKey();
+  const pubKeyBytes = getPubKeyFromPriKey(priKeyBytes);
+  const addressBytes = getAddressFromPriKey(priKeyBytes);
+
+  const privateKey = byteArray2hexStr(priKeyBytes);
+  const publicKey = byteArray2hexStr(pubKeyBytes);
+
+  return {
+    privateKey,
+    publicKey,
+    address: {
+      base58: getBase58CheckAddress(addressBytes),
+      hex: byteArray2hexStr(addressBytes),
+    },
+  };
+}
+
+type RandomParamType = {
+  path?: string;
+};
+
+export function generateRandom() {
+  const account = ethersWallet.createRandom();
+
+  const result = {
+    mnemonic: account.mnemonic,
+    privateKey: account.privateKey,
+    publicKey: account.signingKey.publicKey,
+    address: pkToAddress(account.privateKey.replace(/^0x/, "")),
+  };
+
+  return result;
+}
+
+export function generateAccountWithMnemonic(
+  mnemonic: string,
+  path: string,
+  wordlist: Wordlist | null = null
+) {
+  if (!path) {
+    path = TRON_BIP39_PATH_INDEX_0;
+  }
+  if (!String(path).match(/^m\/44\'\/195\'/)) {
+    throw new Error(INVALID_TRON_PATH_ERROR_MSG);
+  }
+  const account = ethersHDNodeWallet.fromMnemonic(
+    Mnemonic.fromPhrase(mnemonic, path, wordlist)
+  );
+
+  const result = {
+    mnemonic: account.mnemonic,
+    privateKey: account.privateKey,
+    publicKey: account.signingKey.publicKey,
+    address: pkToAddress(account.privateKey.replace(/^0x/, "")),
+  };
+
+  return result;
+}
