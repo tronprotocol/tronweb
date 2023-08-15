9b64472e0a1f6032695fa38c9e08d6fdffa59477
startan <star.tan@tron.network>
2023-08-15 17:07:40 +0800

    test: events

--- test/helpers/config.ts
+++ test/helpers/config.ts
@@ -1,12 +1,10 @@
 import TronWeb from '../setup/node.js';
 
-// const fullHost = 'http://127.0.0.1:' + (process.env.HOST_PORT || 9090);
-// const PK = '';
-const fullHost = 'https://nile.trongrid.io';
-const PK = '5353e96fc5c695d09cb873c927966695e410bf08c8bee1b52154961b18ff7bca';
+const fullHost = 'http://127.0.0.1:' + (process.env.HOST_PORT || 9090);
+// const fullHost = 'https://nile.trongrid.io';
 
 export default {
-    PRIVATE_KEY: PK || (process.env.PRIVATE_KEY as string),
+    PRIVATE_KEY: process.env.PRIVATE_KEY as string,
     CONSUME_USER_RESOURCE_PERCENT: 30,
     FEE_LIMIT: 100000000,
     FULL_NODE_API: fullHost,
--- test/lib/event.test.ts
+++ test/lib/event.test.ts
@@ -15,9 +15,9 @@ describe('TronWeb.lib.event', async function () {
         pks: string[];
     };
     let tronWeb: TronWeb;
-    let contractAddress: Address;
     let contract: IContract;
     let eventLength = 0;
+    let contractAddress: Address;
 
     before(async function () {
         tronWeb = tronWebBuilder.createInstance();
@@ -71,11 +71,9 @@ describe('TronWeb.lib.event', async function () {
                     bytecode:
                         '0x608060405234801561001057600080fd5b50610145806100206000396000f300608060405260043610610041576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063bed7111f14610046575b600080fd5b34801561005257600080fd5b50610091600480360381019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610093565b005b3373ffffffffffffffffffffffffffffffffffffffff167f9f08738e168c835bbaf7483705fb1c0a04a1a3258dd9687f14d430948e04e3298383604051808373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020018281526020019250505060405180910390a250505600a165627a7a7230582033629e2b0bba53f7b5c49769e7e360f2803ae85ac80e69dd61c7bb48f9f401f30029',
                 },
-                // accounts.hex[0]
-                'TVNevinkBb9JytBHhK2ZMsnWX5sWqJu9fx' // for nile
+                accounts.hex[0]
             ),
-            // accounts.pks[0]
-            '5353e96fc5c695d09cb873c927966695e410bf08c8bee1b52154961b18ff7bca' // for nile
+            accounts.pks[0]
         );
 
         contractAddress = result.receipt.transaction.contract_address!;
@@ -137,17 +135,76 @@ describe('TronWeb.lib.event', async function () {
     });
 
     // available on trongrid.
-    describe('#getEventsByContractAddress', async function () {
-        let sender = '41f00c9a48e6d6baca7c36134e7cad7d43a851e7b2';
-        let receiver = '41d4d999298d6bea26bf2bbb32a254362d8b9f9e6b';
-        it.only('should emit an event and wait for it', async function () {
-            this.timeout(60000);
-            await contract.emitNow(receiver, 4000).send(
-                {
-                    from: sender,
-                },
-                '0e1cb9ab46a33201c8debff8cfe40202ca521b0958a5bc45e0d44b514512299b'
+    describe.skip('#getEventsByContractAddress', async function () {
+        let sender = '41d4d999298d6bea26bf2bbb32a254362d8b9f9e6b';
+        let receiver = '41f00c9a48e6d6baca7c36134e7cad7d43a851e7b2';
+        let tronWeb: TronWeb;
+        let contractAddress: Address;
+        let contract: IContract;
+        let eventLength = 0;
+
+        before(async function () {
+            tronWeb = tronWebBuilder.createInstance();
+
+            const result = await broadcaster<CreateSmartContractTransaction>(
+                tronWeb.transactionBuilder.createSmartContract({
+                    abi: [
+                        {
+                            anonymous: false,
+                            inputs: [
+                                {
+                                    indexed: true,
+                                    name: '_sender',
+                                    type: 'address',
+                                },
+                                {
+                                    indexed: false,
+                                    name: '_receiver',
+                                    type: 'address',
+                                },
+                                {
+                                    indexed: false,
+                                    name: '_amount',
+                                    type: 'uint256',
+                                },
+                            ],
+                            name: 'SomeEvent',
+                            type: 'event',
+                        },
+                        {
+                            constant: false,
+                            inputs: [
+                                {
+                                    name: '_receiver',
+                                    type: 'address',
+                                },
+                                {
+                                    name: '_someAmount',
+                                    type: 'uint256',
+                                },
+                            ],
+                            name: 'emitNow',
+                            outputs: [],
+                            payable: false,
+                            stateMutability: 'nonpayable',
+                            type: 'function',
+                        },
+                    ],
+                    bytecode:
+                        '0x608060405234801561001057600080fd5b50610145806100206000396000f300608060405260043610610041576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063bed7111f14610046575b600080fd5b34801561005257600080fd5b50610091600480360381019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610093565b005b3373ffffffffffffffffffffffffffffffffffffffff167f9f08738e168c835bbaf7483705fb1c0a04a1a3258dd9687f14d430948e04e3298383604051808373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020018281526020019250505060405180910390a250505600a165627a7a7230582033629e2b0bba53f7b5c49769e7e360f2803ae85ac80e69dd61c7bb48f9f401f30029',
+                })
             );
+
+            contractAddress = result.receipt.transaction.contract_address!;
+            contract = (await tronWeb.contract().at(contractAddress)) as unknown as IContract;
+        });
+
+        it('should emit an event and wait for it', async function () {
+            this.timeout(180000);
+            await wait(120); // wait for abi syncing.
+            await contract.emitNow(receiver, 4000).send({
+                from: sender,
+            });
             eventLength++;
             let events;
             while (true) {
@@ -155,7 +212,7 @@ describe('TronWeb.lib.event', async function () {
                     event_name: 'SomeEvent',
                     order_by: 'block_timestamp,asc',
                 });
-                if (events.data!.length > 0) {
+                if (events.data!.length === eventLength) {
                     break;
                 }
                 await wait(0.5);
@@ -163,10 +220,8 @@ describe('TronWeb.lib.event', async function () {
 
             const event = events.data![events.data!.length - 1];
 
-            console.log(JSON.stringify(events, null, 2));
-            assert.equal(event.result._receiver.substring(2), accounts.hex[4].substring(2));
-            assert.equal(event.result._sender.substring(2), accounts.hex[3].substring(2));
-            // assert.equal(event.resourceNode, 'fullNode');
+            assert.equal(event.result._receiver.substring(2), receiver.substring(2));
+            assert.equal(event.result._sender.substring(2), sender.substring(2));
         });
     });
 
