546f4f45328751811c52b41fc4558602b5854517
satan <satan@gmail.com>
2024-04-16 16:15:37 +0800

    test: update test for plugin

--- test/lib/plugin.test.ts
+++ test/lib/plugin.test.ts
@@ -1,8 +1,25 @@
 import { assert } from 'chai';
 import tronWebBuilder from '../helpers/tronWebBuilder.js';
-import { TronWeb, Plugin } from '../setup/TronWeb.js';
+import { TronWeb, Plugin, Trx } from '../setup/TronWeb.js';
 import GetNowBlock from '../helpers/GetNowBlock.js';
 import BlockLib from '../helpers/BlockLib.js';
+import { Block } from '../../src/types/APIResponse';
+
+declare namespace globalThis {
+    interface MyTronWeb1 extends TronWeb {
+        trx: {
+            getCurrentBlock(): Promise<Block & { fromPlugin: true }>;
+            getLatestBlock(): Promise<Block & { fromPlugin: true }>;
+            getSomeParameter(): any;
+        } & Trx;
+    }
+
+    interface MyTronWeb2 extends TronWeb {
+        blockLib: {
+            getCurrent(): Promise<Block & { fromPlugin: true }>;
+        }
+    }
+}
 
 describe('TronWeb.lib.plugin', async function () {
     let tronWeb: TronWeb;
@@ -28,26 +45,24 @@ describe('TronWeb.lib.plugin', async function () {
             assert.isTrue(result.plugged.includes('getCurrentBlock'));
             assert.isTrue(result.plugged.includes('getLatestBlock'));
 
-            const result2: any = await tronWeb.trx.getCurrentBlock();
+            const result2 = await (tronWeb as globalThis.MyTronWeb1).trx.getCurrentBlock();
             assert.isTrue(result2.fromPlugin);
             assert.equal(result2.blockID.length, 64);
             assert.isTrue(/^00000/.test(result2.blockID));
 
-            // @ts-ignore
-            const result3: any = await tronWeb.trx.getSomeParameter();
+            const result3 = await (tronWeb as globalThis.MyTronWeb1).trx.getSomeParameter();
             assert.equal(result3, someParameter);
         });
     });
 
     describe('#plug BlockLib into tronWeb at first level', async function () {
         it('should register the plugin and call a method', async function () {
-            let result: any = tronWeb.plugin.register(BlockLib);
+            const result = tronWeb.plugin.register(BlockLib);
             assert.equal(result.libs[0], 'BlockLib');
-            // @ts-ignore
-            result = await tronWeb.blockLib.getCurrent();
-            assert.isTrue(result.fromPlugin);
-            assert.equal(result.blockID.length, 64);
-            assert.isTrue(/^00000/.test(result.blockID));
+            const result2 = await (tronWeb as globalThis.MyTronWeb2).blockLib.getCurrent();
+            assert.isTrue(result2.fromPlugin);
+            assert.equal(result2.blockID.length, 64);
+            assert.isTrue(/^00000/.test(result2.blockID));
         });
 
         it('should not register if tronWeb is instantiated with the disablePlugins option', async function () {
