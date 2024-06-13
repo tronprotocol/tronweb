9cdf702e3bbce6fd16c325eb3cc5ddd904e7f14b
satan <satan@gmail.com>
2024-06-13 15:19:08 +0800

    feat: export providers types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './ABI.js';
 export * from './APIResponse.js';
 export * from './Contract.js';
+export * from './Providers.js';
 export * from './Transaction.js';
 export * from './TransactionBuilder.js';
 export * from './TronWeb.js';
