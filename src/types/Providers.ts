e618734f1e5b9c0325a26860cc77cab6ad89b93c
Satan-web3 <Satan-web3@no-reply.com>
2024-08-06 16:27:06 +0800

    feat: using InstanceType of class to void node16 module error

--- src/types/Providers.ts
+++ src/types/Providers.ts
@@ -1,5 +1,5 @@
 import { RawAxiosRequestHeaders, AxiosHeaders, HeadersDefaults } from 'axios';
-export type HeadersType = RawAxiosRequestHeaders | AxiosHeaders | Partial<HeadersDefaults>;
+export type HeadersType = RawAxiosRequestHeaders | InstanceType<typeof AxiosHeaders> | Partial<HeadersDefaults>;
 export type RequestHeaders = RawAxiosRequestHeaders;
 
 export interface HttpProviderInstance {
