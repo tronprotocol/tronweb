a42f29ceeff34a374dd14f49de3478fcbd1e0b7b
Satan-web3 <Satan-web3@no-reply.com>
2025-03-18 11:18:50 +0800

    chore: change version

--- src/tronweb.ts
+++ src/tronweb.ts
@@ -22,7 +22,7 @@ const DEFAULT_VERSION = '4.7.1';
 
 const FEE_LIMIT = 150000000;
 
-const version = '6.0.2';
+const version = '6.0.3';
 
 function isValidOptions(options: unknown): options is TronWebOptions {
     return (
