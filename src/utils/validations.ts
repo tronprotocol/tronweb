794a37adc05fc3f56bd59bdf417e352b9b05a9f7
Stan202310 <Stan202310@no-reply.com>
2026-01-12 15:39:33 +0800

    fix: solve validator import style

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from 'bignumber.js';
-import { isURL } from 'validator';
+import validator from 'validator';
 import { ADDRESS_PREFIX } from './constants.js';
 import { AbiParamsCommon } from '../types/ABI.js';
 import { IBigNumber } from '../types/TronWeb.js';
@@ -8,7 +8,7 @@ import type { EventQueryDataType, MapEventQueryDataType } from '../types/UtilsTy
 
 export function isValidURL(url: string): boolean {
     if (typeof url !== 'string') return false;
-    return isURL(url.toString(), {
+    return validator.isURL(url.toString(), {
         protocols: ['http', 'https'],
         require_tld: false,
     });
