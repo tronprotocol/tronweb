2b2e039d23d611743104d73e43045d148e80f5b4
Stan202310 <Stan202310@no-reply.com>
2026-06-29 16:17:40 +0800

    test: configure vitest node project with v8 coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,35 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        globals: true,
-        environment: 'node',
-        include: ['test/**/*.test.ts'],
-        testTimeout: 120000,
-        hookTimeout: 120000,
-        fileParallelism: false,
-        pool: 'forks',
-        poolOptions: { forks: { singleFork: true } },
+        coverage: {
+            provider: 'v8',
+            include: ['src/**'],
+            exclude: [
+                'src/protocol/**',
+                'src/types/**',
+                'src/utils/fragments.ts',
+                'src/utils/interface.ts',
+                'test/**',
+                'scripts/**',
+            ],
+            reporter: ['text', 'html'],
+        },
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: 'node',
+                    globals: true,
+                    environment: 'node',
+                    include: ['test/**/*.test.ts'],
+                    testTimeout: 120000,
+                    hookTimeout: 120000,
+                    fileParallelism: false,
+                    pool: 'forks',
+                    poolOptions: { forks: { singleFork: true } },
+                    globalSetup: ['./test/setup/globalSetup.ts'],
+                },
+            },
+        ],
     },
 });
